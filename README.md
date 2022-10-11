e725dd70982377b0f29038ae624a57831bfe217f
RexShack <rexshackuk@gmail.com>
2022-10-11 01:02:20 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# map_cayo_shops
+ 
---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+description 'RSG : Cayo Perico Shops'
+
+this_is_a_map 'yes'
+
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+    "interiorproxies.meta"
+}
